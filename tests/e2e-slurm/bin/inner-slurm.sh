ae76ce180a6580553d3f3af55efa393377ce7306
Austin Macdonald <austin@dartmouth.edu>
2023-12-13 13:24:15 -0500

    add slurm fancy binds and flesh out test script

---
+++ tests/e2e-slurm/bin/inner-slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# each simlink to this file in this dir will be added to the local path but executed inside the running slurm container
+set -eu
+
+RUNNING_SLURM_CONTAINER_NAME=${RUNNING_SLURM_CONTAINER_NAME:=slurm}
+
+#set -x
+cmd="${0##*/}"
+topd=$(readlink -f "$0"  | xargs dirname | xargs dirname)
+
+# This doesnt work because BABs squeue output parsing will fail TODO  file issue
+# echo "Delegating inside: topd=$topd cmd=$cmd"
+
+# --user $USER
+podman exec -it  $RUNNING_SLURM_CONTAINER_NAME "$cmd" "$@"
+
