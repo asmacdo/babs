94828cf6dcea7ae7c236f7a5a57135ff1cca2b5d
Austin Macdonald <austin@dartmouth.edu>
2023-12-14 09:10:20 -0500

    Start slurm and wait until ready

--- tests/e2e-slurm/main.sh
+++ tests/e2e-slurm/main.sh
@@ -18,23 +18,55 @@ TAG=23.11.07 # TODO
 
 FQDN_IMAGE=${REGISTRY}/${HUBUSER}/${REPO}:${TAG}
 
+cleanup () {
+	echo "Shutting down slurm"
+	podman stop slurm
+}
+
 # START SLURM -------------------------------
-	    # -h slurmctl  \ # TODO WHY
-	    # --cap-add sys_admin \ # TODO WHY
-	    # --privileged \ # TODO WHY
 	    # -e "PATH=${MINICONDA_PATH}:$PATH" # This wouldn't work...right? # TODO
 	    # -e "UID=$$(id -u)" \ TODO learn wtf once and for all
 	    # -e "GID=$$(id -g)" \
 	    # -e "USER=$$USER" \
 	    # MINICONDA_PATH needs to be identical in and out?, conda expects? is that true? File RFE upstream?
-podman run --rm \
-	    -v ${PWD}:${PWD}:Z \
-	    -v ${MINICONDA_PATH}:${MINICONDA_PATH}:Z \
-	    ${FQDN_IMAGE}
+podman run -d --rm \
+	--name slurm \
+	--hostname slurmctl  \
+	--cap-add sys_admin \
+	--privileged \
+	-v ${PWD}:${PWD}:Z \
+	-v ${MINICONDA_PATH}:${MINICONDA_PATH}:Z \
+	${FQDN_IMAGE} \
+	tail -f /dev/null # hack to keep it running TODO file issue
 
-export PATH=${PWD}/bin/:${PATH}
+trap cleanup EXIT
 
-sacct
+# Wait for slurm to be up
+# Number of retries
+max_retries=10
+# Delay in seconds
+delay=10
+
+set +e # We need to check the error code and allow failurs until slurm has started up
+export PATH=${PWD}/tests/e2e-slurm/bin/:${PATH}
+for ((i=1; i<=max_retries; i++)); do
+	sacct
+
+	# Check if the command was successful
+	if [ $? -eq 0 ]; then
+		echo "Slurm is up and running!"
+		break
+	else
+		echo "Waiting for Slurm to start... retry $i/$max_retries"
+		sleep $delay
+	fi
+	# exit if max retries reached
+	if [ $i -eq $max_retries ]; then
+		echo "Failed to start Slurm after $max_retries attempts."
+	exit 1
+    fi
+done
+set -e
 
 # TODO babs-init
 
