daf64857a8e9db5d5a9cf52c9851626bdf56a848
Austin Macdonald <austin@dartmouth.edu>
2023-12-14 07:52:42 -0500

    hackout babs install

--- hack/install-babs.sh
+++ hack/install-babs.sh
@@ -1,2 +1,18 @@
 echo "HAVENT IMPLEMENTED YET but are we in the conda env?"
 conda info
+
+
+git config --global user.name "GH Action"
+git config --global user.email "fake@example.com"
+
+conda install -c conda-forge datalad git git-annex
+
+pip install datalad_container
+pip install datalad-osf
+pip install .
+
+
+# TODO How can this be done non-interactively?
+# datalad osf-credentials
+
+pip show babs
