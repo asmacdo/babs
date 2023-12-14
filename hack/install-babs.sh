185628df45a2bd1baf29dfc7ab4fb5fe38faff9c
Austin Macdonald <austin@dartmouth.edu>
2023-12-14 08:09:28 -0500

    ensure babs env or fail

--- hack/install-babs.sh
+++ hack/install-babs.sh
@@ -1,18 +1,17 @@
-echo "HAVENT IMPLEMENTED YET but are we in the conda env?"
-conda info
+set -eu
 
+echo "Confirm that we are in babs environment"
+current_env=$(conda env list | grep '*' | awk '{print $1}')
+if [ "$current_env" != "babs" ]; then
+    echo "Error: This script expects to be run inside a conda env named 'babs'."
+    exit 1
+fi
 
+# TODO does this even need to be configured for gh actions?
 git config --global user.name "GH Action"
 git config --global user.email "fake@example.com"
 
 conda install -c conda-forge datalad git git-annex
-
 pip install datalad_container
-pip install datalad-osf
+# pip install datalad-osf
 pip install .
-
-
-# TODO How can this be done non-interactively?
-# datalad osf-credentials
-
-pip show babs
