c7a3c55cf80bc23939fea19c862adb72e75d6361
Austin <austin@dartmouth.edu>
2024-01-09 10:12:38 -0500

    shellcheck fix setup_container.sh

--- tests/e2e-slurm/setup_container.sh
+++ tests/e2e-slurm/setup_container.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # add that outside user
-groupadd --gid $GID $USER  && useradd --uid $UID --gid $GID $USER
+groupadd --gid "$GID" "$USER"  && useradd --uid $UID --gid "$GID" "$USER"
 
 # Install singularity inside the container
 yum update -y && yum install -y epel-release &&  yum update -y &&  yum install -y singularity-runtime apptainer
@@ -12,9 +12,9 @@ git config user.name > /dev/null || git config --global user.name "e2e slurm"
 git config user.email > /dev/null || git config --global user.email "fake@example.com"
 
 # Setup env to use conda
-cat > /home/$USER/.bashrc << EOF
+cat > /home/"$USER"/.bashrc << EOF
 export MINICONDA_PATH=$MINICONDA_PATH
 echo "HELLO IM SOURCED"
 echo $MINICONDA_PATH
 EOF
-chmod a+x /home/$USER/.bashrc
+chmod a+x /home/"$USER"/.bashrc
