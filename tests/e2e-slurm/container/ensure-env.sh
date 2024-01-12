acc50c5ba796c5ceb97dac591d9425f9b688d264
Austin Macdonald <austin@dartmouth.edu>
2024-01-12 10:43:33 -0500

    fixup: ensure-env shellcheck

--- tests/e2e-slurm/container/ensure-env.sh
+++ tests/e2e-slurm/container/ensure-env.sh
@@ -3,7 +3,8 @@
 # exported for use in inner-slurm.sh
 if [ -z "${MINICONDA_PATH:-}" ]; then
     if hash conda; then
-        MINICONDA_PATH=
+        # We don't need the return value, we already catch the error
+        # shellcheck disable=SC2155
         export MINICONDA_PATH=$(/bin/which conda | xargs dirname | xargs dirname)
     else
         echo "ERROR: must have MINICONDA_PATH set or have 'conda' available"
